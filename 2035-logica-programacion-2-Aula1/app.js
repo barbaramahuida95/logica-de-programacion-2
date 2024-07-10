e66d7110f136cce9c925bc1c2d5b900dbe071800
Barbara Mahuida <barbaramahuida95@gmail.com>
2024-07-10 08:46:34 -0400

    Inicial commit de mi proyecto

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1'); //variable objeto
+titulo.innerHTML = 'Juego del numero Secreto'; 
+
+let parrafo = document.querySelector('p'); 
+parrafo.innerHTML = 'Indica un número del 1 al 10.';
+
+function intentoDeUsuario () {
+    alert('Click desde el Botón');
+}
