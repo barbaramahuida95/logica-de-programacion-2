ae14faab447f3b35b6c4de5e7e378f386b1ee6f9
Barbara Mahuida <barbaramahuida95@gmail.com>
2024-08-01 02:57:00 -0400

    Agrega desafío 4

---
+++ Desafio Aula 4/app.js
@@ -0,0 +1,22 @@
+/*
+Desafío: hora de practicar
+Próxima Actividad
+
+Saber cómo usar las funciones es uno de los conceptos fundamentales en la programación y el desarrollo de software. Las funciones desempeñan un papel crucial en la organización, modularización y reutilización del código.
+
+Con eso en mente, hemos creado una lista de desafíos (no obligatorios) centrados en la práctica para mejorar aún más tu experiencia de aprendizaje. ¡Vamos a practicar entonces!
+
+Desafíos
+Crea una lista vacía llamada "listaGenerica".
+Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
+Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion en orden inverso.
+Crea una función que calcule el promedio de los elementos en una lista de números.
+Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
+Crea una función que devuelva la suma de todos los elementos en una lista.
+Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
+Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno.
+Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado de cada número.
+Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección "Opinión del instructor".
+*/
